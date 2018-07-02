3f079719fd548bd216fab07fc66c48056c66996f
Philippe Moneda <philippemoneda@gmail.com>
2018-07-02 13:52:28 -0300

    adjust

--- http.go
+++ http.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
+	"testing"
 )
 
 //Header is a http header protocol abstraction
@@ -26,7 +28,7 @@ func Post(url string, body interface{}, headers ...Header) (string, error) {
 
 //Get do a GET request
 func Get(url string) (string, error) {
-	if currentContext != nil {
+	if currentContext.mocks != nil {
 		return doRequestMock("GET", url, nil)
 	}
 	if resp, err := http.Get(url); err != nil {
@@ -49,7 +51,7 @@ func GetJSON(url string, obj interface{}) error {
 }
 
 func doRequest(method, url string, body interface{}, headers ...Header) (string, error) {
-	if currentContext != nil {
+	if currentContext.mocks != nil {
 		return doRequestMock(method, url, body)
 	}
 	return httpRequest(method, url, body, headers...)
@@ -90,4 +92,79 @@ func httpRequest(method, url string, body interface{}, headers ...Header) (strin
 	} else {
 		return string(response), nil
 	}
+}
+
+//ReponseMock is mock configure struct
+type ReponseMock struct {
+	Method        string
+	URL           string
+	ReponseBody   string
+	requestBody   string
+	ResponseError error
+	executed      int
+}
+
+//CalledTimes return how many times mock was called
+func (resp *ReponseMock) CalledTimes() int {
+	return resp.executed
+}
+
+//RequestBody returns request body that mock received
+func (resp *ReponseMock) RequestBody() string {
+	return resp.requestBody
+}
+
+var currentContext MockContext
+var mutex sync.Mutex
+
+//MockContext keep current state of mocks
+type MockContext struct {
+	mocks map[string]*ReponseMock
+	test  *testing.T
+}
+
+//RegisterMock register a new mock response to current context
+func (c *MockContext) RegisterMock(mock *ReponseMock) {
+	key := fmt.Sprintf("%s:%s", mock.Method, mock.URL)
+	c.mocks[key] = mock
+}
+
+//Fail fail test
+func (c *MockContext) Fail() {
+	c.test.Fail()
+}
+
+//With creates a new context to register mocks
+func With(t *testing.T, callback func(*MockContext)) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	currentContext.test = t
+	currentContext.mocks = make(map[string]*ReponseMock)
+	callback(&currentContext)
+	currentContext.mocks = nil
+}
+
+func getMock(method, url string) *ReponseMock {
+	key := fmt.Sprintf("%s:%s", method, url)
+	for k, v := range currentContext.mocks {
+		if k == ":" {
+			return v
+		} else if v.URL == "*" && v.Method == method {
+			return v
+		} else if k == key {
+			return v
+		}
+	}
+	return nil
+}
+
+func doRequestMock(method, url string, body interface{}) (string, error) {
+	mock := getMock(method, url)
+	if mock == nil {
+		return "", fmt.Errorf("mock for %s %s not defined exception", method, url)
+	}
+	mock.executed++
+	jsonBody, _ := json.Marshal(body)
+	mock.requestBody = string(jsonBody)
+	return mock.ReponseBody, mock.ResponseError
 }
